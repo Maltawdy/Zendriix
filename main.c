d1b3420df9b275200d51408eda296ab46950d91f
AbdElrhman <maltawdy@hotmil.com>
2019-07-23 10:17:21 +0200

    main file added

---
+++ main.c
@@ -0,0 +1,8 @@
+### That is the main function in pseudo code
+### Created for Zendriix Software 
+### Auther: Abdelrhman.Magdy
+### Email : Maltawdy@hotmail.com
+void main (void)
+{
+	print("Hello World");
+}
